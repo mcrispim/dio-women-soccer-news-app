a17ce3776af8f88374467d31b0462803d6a52044
Mauro Crispim <70303414+mcrispim@users.noreply.github.com>
2022-07-20 14:43:30 -0300

    Abre o link da Notícia em nova activity através de uma Intent

--- app/src/main/java/com/example/womensoccernews/ui/NoticiasAdapter.kt
+++ app/src/main/java/com/example/womensoccernews/ui/NoticiasAdapter.kt
@@ -1,5 +1,7 @@
 package com.example.womensoccernews.ui
 
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -7,7 +9,7 @@ import com.example.womensoccernews.databinding.NoticiaItemBinding
 import com.example.womensoccernews.ui.home.Noticia
 import com.squareup.picasso.Picasso
 
-class NoticiasAdapter(val listaNoticias: List<Noticia>): RecyclerView.Adapter<NoticiasAdapter.NoticiaViewHolder>() {
+class NoticiasAdapter(private val listaNoticias: List<Noticia>): RecyclerView.Adapter<NoticiasAdapter.NoticiaViewHolder>() {
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoticiaViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
@@ -19,9 +21,14 @@ class NoticiasAdapter(val listaNoticias: List<Noticia>): RecyclerView.Adapter<No
                 val noticia = listaNoticias[position]
                 Picasso.get()
                         .load(noticia.imagem)
-                        .into(holder.binding.ivImagem);
+                        .into(holder.binding.ivImagem)
                 holder.binding.tvTitulo.text = noticia.titulo
                 holder.binding.tvTexto.text = noticia.texto
+                holder.binding.btLink.setOnClickListener { _ ->
+                        val openURL = Intent(Intent.ACTION_VIEW)
+                        openURL.data = Uri.parse(noticia.link)
+                        holder.itemView.context.startActivity(openURL)
+                }
         }
 
         override fun getItemCount() = listaNoticias.size
